624aef800c80a882c5631824d96a31e09ca5f64f
Courtney Graham <7194@holbertonstudents.com>
2023-12-15 13:38:45 -0800

    insertion algorithm attempt

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - insertion sort algorithm
+ * @list: doubly linked list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *new = NULL; *temp = NULL;
+
+	if (*list == NULL || list == NULL || (*list)->next == NULL)
+	{
+		break;
+	}
+	new = *list;
+	new = new->next;
+	while (new)
+	{
+		if (new->n < (new->prev)->n)
+		{
+			temp = new->prev;
+			new->next->prev = new->prev;
+			temp->next = new->next;
+			new->next = new->prev;
+			new->prev = new;
+			print_list(*list);
+		}
+		else
+		{
+			new = new->next;
+		}
+	}
+}
