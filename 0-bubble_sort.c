3617b92044bc3e0fd859500a4be531d58a20a52d
Courtney Graham <7194@holbertonstudents.com>
2023-12-15 11:47:45 -0800

    trying something to free array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,17 +8,21 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	for (i = 0; i < size - 1; i++)
+	if (array != NULL)
 	{
-		for (j = 0; j < (size - 1); j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < (size - 1); j++)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+				}
 			}
 		}
 	}
+	free(array);
 }
