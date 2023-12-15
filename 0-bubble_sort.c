cec23002350c4b84eea2507dda220c476aa97fee
Courtney Graham <7194@holbertonstudents.com>
2023-12-15 11:34:13 -0800

    fix integer types for compilation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,9 +5,9 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j, temp;
+	size_t i, j, temp;
 
-	for (i = 0, i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size-1; j++)
 		{
