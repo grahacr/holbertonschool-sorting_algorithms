8e9243fab2de12d1c47d3c7532d813382d41da5f
Courtney Graham <7194@holbertonstudents.com>
2023-12-15 11:29:40 -0800

    header file and bubble sort attempt

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+/**
+ *
+ *
+ */
+void bubble_sort(int *array, size_t size);
+{
+	int i, j, temp;
+
+	for (i = 0, i < size - 1; i++)
+	{
+		for (j = 0; j < size-1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arry[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
