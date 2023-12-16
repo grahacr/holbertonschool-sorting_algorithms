bc70d022a047bb03d9692b2b091b6a3895120e6e
Courtney Graham <7194@holbertonstudents.com>
2023-12-15 17:44:58 -0800

    selection sort attemp

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * selection_sort - sort array using selection algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i; j, min; temp,
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (arr[j] < arr[min])
+			{
+				min = j;
+			}
+		}
+		if (i != min)
+		{
+			temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+}
