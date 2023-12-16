fc0761bfefcf48e64d44fa515b3166bfb690b97c
Courtney Graham <7194@holbertonstudents.com>
2023-12-16 14:47:33 -0800

    fix loop

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,7 +6,8 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min, temp;
+	size_t i, j, min;
+	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -21,6 +22,6 @@ void selection_sort(int *array, size_t size)
 		temp = array[min];
 		array[min] = array[i];
 		array[i] = temp;
+		print_array(array, size);
 	}
-	print_array(array, size);
 }
