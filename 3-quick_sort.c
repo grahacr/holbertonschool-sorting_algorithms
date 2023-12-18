ca75ed5560ecb9f239f9ac90327796cab71c6f76
Courtney Graham <7194@holbertonstudents.com>
2023-12-18 14:43:41 -0800

    quick sort attempt

---
+++ 3-quick_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+/**
+ * int_swap - swap integers
+ * @first: first element to swap
+ * @second: element to swap with first
+ */
+void int_swap(int *first, int *second)
+{
+	int temp;
+
+	temp = *first;
+	*first = *second;
+	second = temp;
+}
+/**
+ * lomuto_part - implement lomuto partition
+ * @array: array to partition
+ * @low: lowest integer in array
+ * @high: highest integer in array
+ * Return: index where partition ended up
+ *
+ */
+int lomuto_part(int array[], int low, int high)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j = low;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			int_swap(&array[i], &array[j]);
+		}
+	}
+	int_swap(&array[i + 1], &array[high]);
+	return (i + 1);
+}
+/**
+ * quick_sort - implemention of quick sort algorithm
+ * @array: array to partition and sort
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	lomuto_part(array, size, 0, size - 1);
+}
--- sort.h
+++ sort.h
@@ -21,4 +21,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int lomuto_part(int array[], int low, int high);
+void int_swap(int *first, int *second);
 #endif
